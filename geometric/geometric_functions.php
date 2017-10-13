2aa5580e028fc224740534ad33d1e1371440568a
Alex DuBois <adubois@alumni.uci.edu>
2017-10-13 17:42:09 -0600

    Created directory structure, blank files for PHP

---
+++ geometric/geometric_functions.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * Geometric equations such as sine, cosine, tangent, their inverses, and other related functions
+ */
---
+++ powers/power_functions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Functions that deal with exponnents, including n-th root and related math equations
+ */
+
+ 
